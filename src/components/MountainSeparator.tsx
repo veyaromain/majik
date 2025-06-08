38eb09ecaea991a19e4f42504a1d5c83d6ba617c
Romain Veya <111903046+veyaromain@users.noreply.github.com>
2025-06-08 18:44:35 +0200

    Créer séparateur montagne réaliste des Alpes

--- src/components/MountainSeparator.tsx
+++ src/components/MountainSeparator.tsx
@@ -19,13 +19,32 @@ const MountainSeparator: React.FC<MountainSeparatorProps> = ({
   return (
     <div className={`w-full ${className}`}>
       <svg
-        viewBox="0 0 1200 50"
-        className="w-full h-[50px]"
+        viewBox="0 0 1200 80"
+        className="w-full h-[80px]"
         preserveAspectRatio="none"
       >
+        {/* Silhouette des Alpes avec de nombreux pics pointus */}
         <path
-          d="M0,50 L0,35 Q120,15 240,25 Q360,35 480,20 Q600,5 720,15 Q840,25 960,10 Q1080,20 1200,30 L1200,50 Z"
+          d="M0,80 L0,65 
+             L50,45 L80,25 L110,40 L140,15 L170,35 L200,10 L230,30 L260,5 L290,25 L320,20 L350,35 L380,12 L410,28 L440,8 L470,22 L500,18 L530,32 L560,6 L590,24 L620,14 L650,38 L680,16 L710,26 L740,11 L770,29 L800,7 L830,21 L860,17 L890,33 L920,9 L950,27 L980,13 L1010,31 L1040,19 L1070,36 L1100,23 L1130,39 L1160,28 L1200,42 
+             L1200,80 Z"
           className={colorClasses[color]}
+        />
+        
+        {/* Couche de montagnes en arrière-plan pour plus de profondeur */}
+        <path
+          d="M0,80 L0,70 
+             L60,55 L120,40 L180,50 L240,35 L300,45 L360,30 L420,40 L480,25 L540,35 L600,20 L660,30 L720,15 L780,25 L840,10 L900,20 L960,5 L1020,15 L1080,25 L1140,35 L1200,45 
+             L1200,80 Z"
+          className={`${colorClasses[color]} opacity-60`}
+        />
+        
+        {/* Troisième couche pour encore plus de réalisme */}
+        <path
+          d="M0,80 L0,75 
+             L100,65 L200,55 L300,60 L400,50 L500,55 L600,45 L700,50 L800,40 L900,45 L1000,35 L1100,40 L1200,50 
+             L1200,80 Z"
+          className={`${colorClasses[color]} opacity-30`}
         />
       </svg>
     </div>
